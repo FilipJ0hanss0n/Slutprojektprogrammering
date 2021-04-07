277d6ae67872c9c7914d26f8b315d4884c897c67
alvin <alja02003@utb.vaxjo.se>
2021-04-07 08:36:59 +0200

    grunder

---
+++ Template/Collision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Collision
+    {
+        static public bool Rect(Rectangle first, Rectangle second)
+        {
+            return first.Intersects(second);
+        }
+    }
+}
